ee80fec382522a8a331452ed2ce7c415d8351df5
Christopher Deo <chris@christopherdeo.com>
2023-05-22 15:44:18 -0500

    Update index.js

--- JavaScript/Assignment/index.js
+++ JavaScript/Assignment/index.js
@@ -6,31 +6,29 @@ const input = require('readline-sync');
 let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let correctAnswer =["Paris", 4, "Electric", "Humpdaayy!!", true] ;
+let candidateAnswer= [];
+let questions = ["What is the capital of France?", "What is 2*2?", "What kind of car is worthless?", "Mike, Mike, what daaaay is iiiiitt?", "T/F Typescript rules and JS drools!"];
+let correctAnswers = [];
+let candidateAnswers = [];
 
 
 function askForName() {
     // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("What is your name? ");
+candidateName = input.question("What is your name? ")
 }
 
 function askQuestion() {
     // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-    console.log(questions[0]);
+    for(let i = 0; i < questions.length; i++){
+    console.log(questions[i]);
     candidateAnswer = input.question("What is your answer? ");
-
+    }
 }
 
 function gradeQuiz(candidateAnswers) {
     // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
     let grade;
-
 
     return grade;
 }
@@ -38,7 +36,6 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
     askForName();
     // TODO 1.1c: Ask for candidate's name //
-
     askQuestion();
     gradeQuiz(this.candidateAnswers);
 }
